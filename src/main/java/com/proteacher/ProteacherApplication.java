0e95ff5d6d0fb47b189b8cd42def48c73a65df94
Ярослав Гулага <hulaga@MacBook-Pro-Aroslav.local>
2017-02-27 10:46:56 +0200

    Add security

--- src/main/java/com/proteacher/ProteacherApplication.java
+++ src/main/java/com/proteacher/ProteacherApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProteacherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProteacherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProteacherApplication.class, args);
+    }
 }
